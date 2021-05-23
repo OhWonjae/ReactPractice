ab539c34ec033d9d6109d5993f0a275a04d5e277
OhWonjae <dhdnjswo5000@naver.com>
2021-05-24 01:25:32 +0900

    commit

--- src/views/Ch03PropsAndState/Exam01Props/ClassTypeProps.js
+++ src/views/Ch03PropsAndState/Exam01Props/ClassTypeProps.js
@@ -2,9 +2,6 @@
 import React from "react";
 import PropTypes from "prop-types"
 class ClassTypeProps extends React.Component{
-     constructor(props){
-         super(props)
-     }
     render(){
         const {name, version, children} = this.props;
         return(
